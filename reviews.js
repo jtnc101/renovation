96a60bb987b4ffb413c5dc10d364f53b0e485605
jtnc101 <71797572+jtnc101@users.noreply.github.com>
2021-08-05 06:34:03 -0400

    aug5

---
+++ reviews.js
@@ -0,0 +1,15 @@
+// import "helpers";
+
+document.addEventListener('DOMContentLoaded', 
+
+    () => { 
+        document.querySelector(".fa-bars").onclick = changeVerticalMenuBar;
+        document.querySelector(".fa-times").onclick = changeVerticalMenuBar;
+        navLinks = document.querySelectorAll(".vert-nav-link");
+        navLinks.forEach( element => (element.onclick = changeVerticalMenuBar) );
+
+
+        
+                                
+    });
+
